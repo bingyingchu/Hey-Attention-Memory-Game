4c2c9ad9bc5d87db20e55d8597d08a96406c8095
Glitch (hello-webpage) <none>
2021-03-13 01:08:09 +0000

    🍛📒 Checkpoint ./script.js:29678232/820

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// global constants
+const clueHoldTime = 1000; //how long to hold each clue's light/sound
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+
 //Global Variables
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
 var volume = 0.5; // must be between 0.0 and 0.1
+
 
 function startGame(){
   //initialize game variables
@@ -28,31 +34,51 @@ const freqMap = {
   4: 466.2
 }
 function playTone(btn,len){ 
-  o.frequency.value = freqMap[btn]
-  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-  tonePlaying = true
+  o.frequency.value = freqMap[btn];
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025);
+  tonePlaying = true;
   setTimeout(function(){
-    stopTone()
-  },len)
+    stopTone();
+  },len);
 }
 function startTone(btn){
   if(!tonePlaying){
-    o.frequency.value = freqMap[btn]
-    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-    tonePlaying = true
+    o.frequency.value = freqMap[btn];
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025);
+    tonePlaying = true;
   }
 }
 function stopTone(){
-    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
-    tonePlaying = false
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025);
+    tonePlaying = false;
 }
 
 //Page Initialization
 // Init Sound Synthesizer
-var context = new AudioContext()
-var o = context.createOscillator()
-var g = context.createGain()
-g.connect(context.destination)
-g.gain.setValueAtTime(0,context.currentTime)
-o.connect(g)
-o.start(0)
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);
+g.gain.setValueAtTime(0,context.currentTime);
+o.connect(g);
+o.start(0);
+
+function lightButton(btn){
+  document.getElementById("buttton"+btn).classList.add("lit");
+}
+
+function clearButton(btn){
+  document.getElementById("button"+btn).classList.remove("lit");
+}
+
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+
+function playClueSequence(){
+  
+}
